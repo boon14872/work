500b6b60085b09e9d2e4fc394b55e0bcf91fb76f
BOONKUNG <boon14872@gmail.com>
2022-06-29 18:57:02 +0700

    code detail

--- src/js/main.js
+++ src/js/main.js
@@ -99,32 +99,44 @@ var data_raw = [{
     },
 ]}];
 
-var data = data_raw[0].data;
-var data_length = data.length;
-var number = 0;
+var data = data_raw[0].data;  // data for use
+var data_length = data.length; // length of data
+var number = 0; // number now of question
 
 $(document).ready(function (){
-    updateq(data, number);
+    updateq(data, number); // update first question
+    // check if button is clicked
     $('button').on('click', function() {
-        if (number >= data_length) {
-            return;
+        if (number >= data_length-1) { // check if question end
+            return; // do nothing
         }
-        number++;
-        updateq(data, number);
+        number++; // increment number
+        updateq(data, number); // update question
     });
 });
 
+
+// function for update question and choices
 function updateq(data, id) {
-    const question = $('div#header > div');
-    $(question[0]).text(data[id].first);
-    $(question[1]).text(data[id].operator);
-    $(question[2]).text(data[id].last);
+    const question = $('div#header > div'); // question element
+    $(question[0]).text(data[id].first); // question first number
+    $(question[1]).text(data[id].operator); // question operator
+    $(question[2]).text(data[id].last); // question last number
     //console.log(question);
-    const choice = $('div#choice_box > button');
-    for (let i = 0; i < choice.length; i++) {
-        if ($(choice[i]).attr('id') == i && data[id].choices[i].id == i) {
-            $(choice[i]).text(data[id].choices[i].text);
+    const choice = $('div#choice_box > button'); // button element
+    for (let i = 0; i < choice.length; i++) { // loop for change choice data
+        if ($(choice[i]).attr('id') == i && data[id].choices[i].id == i) { // check choice id and data id
+            $(choice[i]).text(data[id].choices[i].text); // change choice data
         }
     }
-    $('div#progress_bar > div').css("width",((number+1)*(100/(data_length)))+'%');
+    $('div#progress_bar > div').css("width",((number+1)*(100/(data_length)))+'%'); // update progress bar
+}
+
+// time counter
+var totalSeconds = 0; // total number of seconds
+setInterval(setTime, 1000); // set time counter
+
+// function for time counter
+function setTime() {
+  ++totalSeconds; // increment time counter
 }
